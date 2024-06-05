5c3f1cdb34ba6ab861ad9ef13ddccdcc6c862192
Alex1445085 <n1445085@yandex.ru>
2024-06-05 17:11:14 +0300

    tasks 1, 2

--- src/Main.java
+++ src/Main.java
@@ -17,5 +17,32 @@ public class Main {
                 break;
             }
         }
+  // 2
+        System.out.println("2.");
+        clientOS = 0;
+        int clientDeviceYear = 2017;
+        switch (clientOS) {
+            case 0: {
+                if (clientDeviceYear < 2015) {
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                }
+                else {
+                    System.out.println("Установите версию приложения для iOS по ссылке");
+                }
+                break;
+            }
+            case 1: {
+                if (clientDeviceYear < 2015) {
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                }
+                else {
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                }
+                break;
+            }
+            default: {
+                break;
+            }
+        }
     }
 }
